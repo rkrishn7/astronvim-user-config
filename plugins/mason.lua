35068f8e3fe18ddf53531c9463abe60372001bad
rkrishn7 <rkrishn7@users.noreply.github.com>
2023-09-28 22:53:08 -0700

    update ensure installed

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,11 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "rust_analyzer",
+        "tsserver",
+        "eslint",
+        "pylsp"
       })
     end,
   },
@@ -18,8 +22,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "stylua",
       })
     end,
   },
